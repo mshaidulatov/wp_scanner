af44070f46e082297f8e72efb2d8459f7db1d3c4
Maxim Olefirenko <m.olefirenko@gmail.com>
2016-03-15 18:05:34 +0200

    изменена версия плагина

--- wp_scanner.php
+++ wp_scanner.php
@@ -3,7 +3,7 @@
     Plugin Name: WP Scan Site
     Description: Plugin for checking site changes
     Author: M. Olefirenko
-    Version: 1.3
+    Version: 1.3.1
     */
 
 function init_admin_page() {
